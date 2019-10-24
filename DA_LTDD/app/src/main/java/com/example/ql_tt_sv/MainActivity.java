2257dcaddb120e9bb8020668d39ac5439e34668d
Bảo <54348426+HoaiBaoHuynh@users.noreply.github.com>
2019-10-24 15:59:05 +0700

    news

--- DA_LTDD/app/src/main/java/com/example/ql_tt_sv/MainActivity.java
+++ DA_LTDD/app/src/main/java/com/example/ql_tt_sv/MainActivity.java
@@ -2,24 +2,38 @@ package com.example.ql_tt_sv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtuser,edtpassword;
     Button btn_login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_main);
+        setContentView(R.layout.ttsv_form);
         Anhxa();
+        convetpicture();
         String hoten = "Huynh Hoai Bao";
         Log.d("HUYNHBAO",hoten);
 
     }
+
+    private void convetpicture() {
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.pikalong);
+        Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
+
+        ImageView circularImageView = (ImageView) findViewById(R.id.imageView);
+        circularImageView.setImageBitmap(circularBitmap);
+    }
+
 
     private void Anhxa() {
         edtuser = (EditText)findViewById(R.id.edittextuser);
