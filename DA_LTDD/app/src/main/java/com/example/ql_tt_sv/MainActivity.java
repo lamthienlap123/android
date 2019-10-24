86ab5fbfc1e023ed985d0cc2b1a674613bb98a72
Bảo <54348426+HoaiBaoHuynh@users.noreply.github.com>
2019-10-24 14:02:02 +0700

    login

--- DA_LTDD/app/src/main/java/com/example/ql_tt_sv/MainActivity.java
+++ DA_LTDD/app/src/main/java/com/example/ql_tt_sv/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.ql_tt_sv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtuser,edtpassword;
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_main);
+        Anhxa();
+        String hoten = "Huynh Hoai Bao";
+        Log.d("HUYNHBAO",hoten);
+
+    }
+
+    private void Anhxa() {
+        edtuser = (EditText)findViewById(R.id.edittextuser);
+        edtpassword = (EditText)findViewById(R.id.edittextpassword);
+        btn_login = (Button)findViewById(R.id.btn_login);
+
     }
 }
